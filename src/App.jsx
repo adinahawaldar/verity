635027f3facdaea6b954ba52ed0a82c6f173ec68
adinahawaldar <adinahawaldar006@gmail.com>
2026-02-07 23:46:56 +0530

    added usecases

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import About from './Components/About';
 import MisinformationCases from './Components/MisinformationCases';
 import Verification from "./Components/Verification";
 import Cases from './Components/Cases';
+import UseCases from './Components/UseCases';
 import Categories from "./Components/Categories";
 import Footer from './Components/Footer'
 export default function App() {
@@ -16,6 +17,7 @@ export default function App() {
     <MisinformationCases/>
     <Verification/>
     {/* <Cases/> */}
+    <UseCases/>
     <Categories/>
     <Footer/>
     </>
